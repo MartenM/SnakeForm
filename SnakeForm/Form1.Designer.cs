76615b902f88f21394ecb20c6d1454fe7d218ffd
MartenM <marten.struijk@gmail.com>
2017-09-13 15:45:17 +0200

    Game finished v1.0

--- SnakeForm/Form1.Designer.cs
+++ SnakeForm/Form1.Designer.cs
@@ -29,30 +29,156 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel_game = new System.Windows.Forms.Panel();
+            this.label_score = new System.Windows.Forms.Label();
+            this.panel_reset = new System.Windows.Forms.Panel();
+            this.panel_start = new System.Windows.Forms.Panel();
+            this.panel_header = new System.Windows.Forms.Panel();
+            this.label_speed = new System.Windows.Forms.Label();
+            this.label_speed_minus = new System.Windows.Forms.Label();
+            this.label_speed_show = new System.Windows.Forms.Label();
+            this.label_speed_plus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // panel_game
+            // 
+            this.panel_game.Location = new System.Drawing.Point(10, 82);
+            this.panel_game.Name = "panel_game";
+            this.panel_game.Size = new System.Drawing.Size(250, 250);
+            this.panel_game.TabIndex = 0;
+            this.panel_game.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label_score
+            // 
+            this.label_score.AutoSize = true;
+            this.label_score.Font = new System.Drawing.Font("Endless Boss Battle", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_score.ForeColor = System.Drawing.Color.Lime;
+            this.label_score.Location = new System.Drawing.Point(12, 63);
+            this.label_score.Name = "label_score";
+            this.label_score.Size = new System.Drawing.Size(113, 16);
+            this.label_score.TabIndex = 2;
+            this.label_score.Text = "Score: 0000";
+            this.label_score.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel_reset
+            // 
+            this.panel_reset.BackgroundImage = global::SnakeForm.Properties.Resources.reset;
+            this.panel_reset.Location = new System.Drawing.Point(160, 369);
+            this.panel_reset.Name = "panel_reset";
+            this.panel_reset.Size = new System.Drawing.Size(100, 30);
+            this.panel_reset.TabIndex = 4;
+            this.panel_reset.Click += new System.EventHandler(this.panel_reset_Click);
+            // 
+            // panel_start
+            // 
+            this.panel_start.BackgroundImage = global::SnakeForm.Properties.Resources.play;
+            this.panel_start.Location = new System.Drawing.Point(10, 369);
+            this.panel_start.Name = "panel_start";
+            this.panel_start.Size = new System.Drawing.Size(100, 30);
+            this.panel_start.TabIndex = 3;
+            this.panel_start.Click += new System.EventHandler(this.panel_start_Click);
+            // 
+            // panel_header
+            // 
+            this.panel_header.BackgroundImage = global::SnakeForm.Properties.Resources.snake;
+            this.panel_header.Location = new System.Drawing.Point(10, 10);
+            this.panel_header.Name = "panel_header";
+            this.panel_header.Size = new System.Drawing.Size(250, 50);
+            this.panel_header.TabIndex = 1;
+            // 
+            // label_speed
+            // 
+            this.label_speed.AutoSize = true;
+            this.label_speed.Font = new System.Drawing.Font("Endless Boss Battle", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_speed.ForeColor = System.Drawing.Color.Lime;
+            this.label_speed.Location = new System.Drawing.Point(12, 344);
+            this.label_speed.Name = "label_speed";
+            this.label_speed.Size = new System.Drawing.Size(45, 12);
+            this.label_speed.TabIndex = 6;
+            this.label_speed.Text = "Speed";
+            // 
+            // label_speed_minus
+            // 
+            this.label_speed_minus.AutoSize = true;
+            this.label_speed_minus.Font = new System.Drawing.Font("Endless Boss Battle", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_speed_minus.ForeColor = System.Drawing.Color.Lime;
+            this.label_speed_minus.Location = new System.Drawing.Point(64, 344);
+            this.label_speed_minus.Name = "label_speed_minus";
+            this.label_speed_minus.Size = new System.Drawing.Size(12, 12);
+            this.label_speed_minus.TabIndex = 7;
+            this.label_speed_minus.Text = "<";
+            this.label_speed_minus.Click += new System.EventHandler(this.label_speed_minus_Click);
+            // 
+            // label_speed_show
+            // 
+            this.label_speed_show.AutoSize = true;
+            this.label_speed_show.Font = new System.Drawing.Font("Endless Boss Battle", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_speed_show.ForeColor = System.Drawing.Color.Lime;
+            this.label_speed_show.Location = new System.Drawing.Point(77, 344);
+            this.label_speed_show.Name = "label_speed_show";
+            this.label_speed_show.Size = new System.Drawing.Size(13, 12);
+            this.label_speed_show.TabIndex = 8;
+            this.label_speed_show.Text = "0";
+            this.label_speed_show.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label_speed_plus
+            // 
+            this.label_speed_plus.AutoSize = true;
+            this.label_speed_plus.Font = new System.Drawing.Font("Endless Boss Battle", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_speed_plus.ForeColor = System.Drawing.Color.Lime;
+            this.label_speed_plus.Location = new System.Drawing.Point(96, 344);
+            this.label_speed_plus.Name = "label_speed_plus";
+            this.label_speed_plus.Size = new System.Drawing.Size(12, 12);
+            this.label_speed_plus.TabIndex = 9;
+            this.label_speed_plus.Text = ">";
+            this.label_speed_plus.Click += new System.EventHandler(this.label_speed_plus_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 311);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(274, 411);
+            this.Controls.Add(this.label_speed_plus);
+            this.Controls.Add(this.label_speed_show);
+            this.Controls.Add(this.label_speed_minus);
+            this.Controls.Add(this.label_score);
+            this.Controls.Add(this.label_speed);
+            this.Controls.Add(this.panel_reset);
+            this.Controls.Add(this.panel_start);
+            this.Controls.Add(this.panel_header);
+            this.Controls.Add(this.panel_game);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(290, 450);
+            this.MinimumSize = new System.Drawing.Size(290, 450);
             this.Name = "Form1";
             this.Text = "Snake";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel_game;
+        private System.Windows.Forms.Panel panel_header;
+        private System.Windows.Forms.Label label_score;
+        private System.Windows.Forms.Panel panel_start;
+        private System.Windows.Forms.Panel panel_reset;
+        private System.Windows.Forms.Label label_speed;
+        private System.Windows.Forms.Label label_speed_minus;
+        private System.Windows.Forms.Label label_speed_show;
+        private System.Windows.Forms.Label label_speed_plus;
     }
 }
 
--- SnakeForm/Form1.cs
+++ SnakeForm/Form1.cs
@@ -16,14 +16,20 @@ namespace SnakeForm
         public int height;
         public int width;
 
-        public int block_height = 250 / 10;
-        public int block_width = 250 / 10;
+        public int block_height = 250 / 20;
+        public int block_width = 250 / 20;
 
-        public Keys last_pressed = Keys.W;
+        public Keys last_pressed = Keys.S;
 
-        public Point[] snake = new Point[] { new Point(1, 1) };
+        public Point[] snake = new Point[] { new Point(8, 8), new Point(8, 9), new Point(8, 10) };
 
-        private Graphics g;
+        public Boolean isPlaying = false;
+
+        public int score = 0;
+
+        public int speed;
+
+        public Point food;
         
         public Form1()
         {
@@ -35,24 +41,18 @@ namespace SnakeForm
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            timer1.Start();
+            
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            g = e.Graphics;
-            draw(g);
+            draw();
         }
 
-        public void draw(Graphics g)
+        public void draw()
         {
-            g.FillRectangle(Brushes.Black, 10, 10, width, height);
-            g.DrawRectangle(Pens.Cyan, 10, 10, width, height);
-
-            foreach (Point p in snake)
-            {
-                g.FillRectangle(Brushes.White, 10 + p.X * block_width, 10 + p.Y * block_height, block_width, block_height);
-            }
+            panel_game.Refresh();
+            label_score.Text = "Score: " + score;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -70,24 +70,26 @@ namespace SnakeForm
                 x_mod++;
             } else if (last_pressed == Keys.W)
             {
-                y_mod++;
+                y_mod--;
             } else if (last_pressed == Keys.S)
             {
-                y_mod--;
+                y_mod++;
             }
             
-
-            for (int i = snake.Length - 1; i > 0; i++)
-            {
-                snake[i] = snake[i - 1];
-            }
-
             oldpoint.X += x_mod;
             oldpoint.Y += y_mod;
 
+            for (int i = 0; i < snake.Length - 1; i++)
+            {
+                snake[snake.Length - 1 - i] = snake[snake.Length - 2 - i];
+            }
+
             snake[0] = oldpoint;
 
-            draw(g);
+            eat();
+           
+
+            draw();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -96,6 +98,167 @@ namespace SnakeForm
             {
                 last_pressed = e.KeyCode;
             }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (checkDeath())
+            {
+                e.Graphics.DrawString("You died...", SystemFonts.MenuFont, Brushes.Red, 100, 75);
+                e.Graphics.DrawString("Score: " + score, SystemFonts.MenuFont, Brushes.Yellow, 100, 90);
+                e.Graphics.DrawIcon(SystemIcons.Warning, 70, 75);
+                timer1.Stop();
+                return;
+            }
+
+
+            e.Graphics.FillRectangle(Brushes.DarkGray, 0, 0, 250, 250);
+            e.Graphics.DrawRectangle(Pens.DarkCyan, 0, 0, 250, 250);
+
+            if(food != null)
+            {
+                e.Graphics.FillRectangle(Brushes.Green, food.X * block_width, food.Y * block_height, block_width, block_height);
+            }
+
+            for(int i = 0; i < snake.Length; i++)
+            {
+                Point p = snake[i];
+
+                if (i != 0)
+                {
+                    e.Graphics.FillRectangle(Brushes.White, p.X * block_width, p.Y * block_height, block_width, block_height);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(Brushes.AntiqueWhite, p.X * block_width, p.Y * block_height, block_width, block_height);
+                }
+            }
+        }
+
+        public void hideButtons()
+        {
+            if (isPlaying)
+            {
+                panel_start.Visible = false;
+                panel_reset.Visible = true;
+                
+            }
+            else
+            {
+                panel_start.Visible = true;
+                panel_reset.Visible = false;
+            }
+
+            label_speed.Visible = !isPlaying;
+            label_speed_minus.Visible = !isPlaying;
+            label_speed_plus.Visible = !isPlaying;
+            label_speed_show.Visible = !isPlaying;
+        }
+
+        //buttons
+
+        private void panel_start_Click(object sender, EventArgs e)
+        {
+            if (isPlaying)
+            {
+                panel_start.Visible = false;
+                return;
+            }
+
+            if(100 - (speed * 10) <= 0)
+            {
+                speed = 9;
+                label_speed_show.Text = speed.ToString();
+                return;
+            }
+
+            timer1.Interval = 100 - (speed * 10);
+
+            isPlaying = true;
+            hideButtons();
+            createFood();
+            timer1.Start();
+        }
+
+        private void panel_reset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            snake = new Point[] { new Point(8, 8), new Point(8, 9), new Point(8, 10) };
+            isPlaying = false;
+            draw();
+            hideButtons();
+        }
+
+
+        //Game logic
+        private Boolean checkDeath()
+        {
+            for(int i = 1; i < snake.Length - 1; i++)
+            {
+                if(snake[0].X == snake[i].X && snake[0].Y == snake[i].Y)
+                {
+                    return true;
+                }
+            }
+
+            if(snake[0].X < 0 || snake[0].Y < 0)
+            {
+                return true;
+            }
+
+            if(snake[0].X > 20 || snake[0].Y > 20)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void eat()
+        {
+            if(snake[0].X == food.X && snake[0].Y == food.Y)
+            {
+                score += 100 + (speed * 50);
+                Point[] temp = new Point[snake.Length + 1];
+
+                for(int i = 0; i < snake.Length; i++)
+                {
+                    temp[i] = snake[i];
+                }
+
+                snake = temp;
+                snake[snake.Length - 1] = snake[snake.Length - 2];
+                createFood();
+            }
+        }
+
+        private void createFood()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(0, 20);
+            int y = rnd.Next(0, 20);
+            food = new Point(x, y);
+        }
+
+        private void label_speed_minus_Click(object sender, EventArgs e)
+        {
+            speed--;
+
+            if (speed < 0)
+            {
+                speed = 0;
+                MessageBox.Show("Speed cannot be lower then 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return;
+            }
+
+            label_speed_show.Text = speed.ToString();
+        }
+
+        private void label_speed_plus_Click(object sender, EventArgs e)
+        {
+            speed++;   
+
+            label_speed_show.Text = speed.ToString();          
         }
     }
 }
